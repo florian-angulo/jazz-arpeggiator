7be468e8a1597c279fc84abdf713c295f8fcaa3f
vscvl <73337791+vscvl@users.noreply.github.com>
2021-01-22 13:34:37 +0100

    MAJ_cuda_available

--- VAE_MNIST.py
+++ VAE_MNIST.py
@@ -12,7 +12,15 @@ from torchvision import datasets, transforms
 from torchvision.utils import save_image
 
 
-device = torch.device("cpu")
+
+if torch.cuda.is_available():  
+  device = torch.device("cuda:0")
+else:  
+  device = torch.device("cpu")
+  
+print('using',device)
+
+
 class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
@@ -45,9 +53,9 @@ class VAE(nn.Module):
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar, beta):
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
-
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    #KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    KLD = 0.5 * torch.sum(logvar.exp() + mu.pow(2) - 1 - logvar)
 
     return BCE + beta*KLD
 
@@ -55,7 +63,8 @@ def loss_function(recon_x, x, mu, logvar, beta):
 def train(epoch):
     model.train()
     train_loss = 0
-    beta = epoch/epochs
+    beta = 0.5
+    
     for batch_idx, (data, _) in enumerate(mnist_trainset):
         data = data.to(device)
         optimizer.zero_grad()
@@ -64,6 +73,7 @@ def train(epoch):
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
+        
         if batch_idx % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(mnist_trainset.dataset),
@@ -77,42 +87,52 @@ def train(epoch):
 def test(epoch):
     model.eval()
     test_loss = 0
-    beta = epoch/epochs
+    beta = 0.5
+    
     with torch.no_grad():
+        
         for i, (data, _) in enumerate(mnist_testset):
             data = data.to(device)
             recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+            test_loss += loss_function(recon_batch, data, mu, logvar, beta)  \
+                                      .item()
+                                      
             if i == 0:
                 n = min(data.size(0), 8)
                 comparison = torch.cat([data[:n],
-                                      recon_batch.view(batch_size, 1, 28, 28)[:n]])
-                save_image(comparison.cpu(),
-                          'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+                                      recon_batch.view(batch_size, 1, 28, 28)\
+                                      [:n]])
+                save_image(comparison.to(device),
+                          'results/reconstruction_' + str(epoch) + '.png',
+                          nrow=n)
 
     test_loss /= len(mnist_testset.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
 
+
 epochs = 10
 batch_size = 64
 log_interval = 100
+
 mnist_trainset = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=True, download=True,
                    transform=transforms.ToTensor()),
-    batch_size=batch_size, shuffle=True)
-# mnist_testset = test_loader = torch.utils.data.DataLoader(
-#     datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-#     batch_size=batch_size, shuffle=True)
+                   batch_size=batch_size, shuffle=True)
 
-# model = VAE().to(device)
-# optimizer = optim.Adam(model.parameters(), lr=7e-4)
+mnist_testset = test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
+                  batch_size=batch_size, shuffle=True)
 
-# if __name__ == "__main__":
-#     for epoch in range(1, epochs + 1):
-#         train(epoch)
-#         test(epoch)
-#         with torch.no_grad():
-#             sample = torch.randn(64, 40).to(device)
-#             sample = model.decode(sample).cpu()
-#             save_image(sample.view(64, 1, 28, 28),
-#                        'results/sample_' + str(epoch) + '.png')
+model = VAE().to(device)
+optimizer = optim.Adam(model.parameters(), lr=7e-4)
+
+if __name__ == "__main__":
+    for epoch in range(1, epochs + 1):
+        train(epoch)
+        test(epoch)
+        with torch.no_grad():
+            sample = torch.randn(64, 40).to(device)
+            sample = model.decode(sample).to(device)
+            save_image(sample.view(64, 1, 28, 28),
+                        'results/sample_' + str(epoch) + '.png')
+
